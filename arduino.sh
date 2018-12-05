daf5e87f0c985c2cd027e794452894053bd98209
Olivier WATTÉ <olivier@biolo.tech>
2018-12-04 21:38:25 -0400

    update to IDE Arduino v1.8.7

--- arduino.sh
+++ arduino.sh
@@ -1,3 +1,13 @@
+# @Author: Olivier Watté <user>
+# @Date:   2018-12-04T20:15:30-04:00
+# @Email:  owatte@ipeos.com
+# @Last modified by:   user
+# @Last modified time: 2018-12-04T21:34:01-04:00
+# @License: GPLv3
+# @Copyright: IPEOS I-Solutions
+
+
+
 #!/bin/bash
 # Starts the Arduino IDE using the docker image.
 # see also: ...
@@ -15,6 +25,5 @@ docker run \
     -v $HOME/topics:/topics \
     -v $HOME/topics/arduino:/home/developer/Arduino \
     --name arduino \
-    tombenke/darduino:v1.8.5 \
+    darduino:v1.8.7 \
     arduino
-
